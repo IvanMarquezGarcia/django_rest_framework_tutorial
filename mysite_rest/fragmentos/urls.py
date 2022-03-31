83a4e0917111358543fe80d56ca1d4cdf6ecf39e
Iván <ivanmarquezgarcia00@gmail.com>
2022-03-31 12:27:33 +0200

    DRF tutorial 02

--- mysite_rest/fragmentos/urls.py
+++ mysite_rest/fragmentos/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from fragmentos import views
 
 
@@ -8,3 +10,5 @@ urlpatterns = [
     path('fragmentos/', views.fragmento_lista),
     path('fragmentos/<int:pk>/', views.fragmento_detalles),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- mysite_rest/fragmentos/views.py
+++ mysite_rest/fragmentos/views.py
@@ -4,6 +4,12 @@ from django.http import HttpResponse, JsonResponse
 
 from django.views.decorators.csrf import csrf_exempt
 
+from rest_framework import status
+
+from rest_framework.decorators import api_view
+
+from rest_framework.response import Response
+
 from rest_framework.parsers import JSONParser
 
 from fragmentos.models import Fragmento
@@ -12,42 +18,46 @@ from fragmentos.serializers import SerializadorFragmento
 
 # Create your views here.
 
-@csrf_exempt
-def fragmento_lista(request):
+# funcionalidad de api_view para indicar una lista de métodos http que la vista debe responder
+@api_view(['GET', 'POST'])
+# evitar el uso de CSRF tokens para esta vista
+#@csrf_exempt
+def fragmento_lista(request, format = None):
     # lista todos los fragmentos o crea uno nuevo
     if request.method == 'GET':
         fragmentos = Fragmento.objects.all()
         serializador = SerializadorFragmento(fragmentos, many = True)
-        return JsonResponse(serializador.data, safe = False)
+        return Response(serializador.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializador = SerializadorFragmento(data = data)
+        serializador = SerializadorFragmento(data = request.data)
         if serializador.is_valid():
             serializador.save()
-            return JsonResponse(serializador.data, status = 201)
-        return JsonResponse(serializador.errors, status = 400)
+            return Response(serializador.data, status = status.HTTP_201_CREATED)
+        return Response(serializador.errors, status = status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-def fragmento_detalles(request, pk):
+# funcionalidad de api_view para indicar una lista de métodos http que la vista debe responder
+@api_view(['GET', 'PUT', 'DELETE'])
+# evitar el uso de CSRF tokens para esta vista
+#@csrf_exempt#@csrf_exempt # evitar el uso de CSRF tokens para esta vista
+def fragmento_detalles(request, pk, format = None):
     # Muestra, actualiza o elimina un fragmento
     try:
         fragmento = Fragmento.objects.get(pk = pk)
     except Fragmento.DoesNotExist:
-        return HttpResponse(status = 404)
+        return HttpResponse(status = status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializador = SerializadorFragmento(fragmento)
-        return JsonResponse(serializador.data)
+        return Response(serializador.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializador = SerializadorFragmento(fragmento, data = data)
+        serializador = SerializadorFragmento(fragmento, data = request.data)
         if serializador.is_valid():
             serializador.save()
-            return JsonResponse(serializador.data)
-        return JsonResponse(serializador.errors, status = 400)
+            return Response(serializador.data)
+        return Response(serializador.errors, status = status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         fragmento.delete()
-        return HttpResponse(status=204)
+        return HttpResponse(status = status.HTTP_204_NO_CONTENT)
