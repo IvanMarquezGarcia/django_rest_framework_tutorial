34ad0b6ced6bb7798ccd68aa48fb80e9c93dc11c
Iván <ivanmarquezgarcia00@gmail.com>
2022-03-31 13:43:22 +0200

    Vistas basadas en Clases

--- mysite_rest/fragmentos/urls.py
+++ mysite_rest/fragmentos/urls.py
@@ -4,11 +4,22 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 from fragmentos import views
 
+#	-- URLS PARA VISTAS BASADAS EN CLASES --
+urlpatterns = [
+    path('', views.FragmentoLista.as_view()),
+    path('fragmentos/', views.FragmentoLista.as_view()),
+    path('fragmentos/<int:pk>/', views.FragmentoDetalles.as_view()),
+]
 
+# ------------------------------------------------------------------------------------
+
+'''
+#	-- URLS PARA VISTAS BASADAS EN FUNCIONES --
 urlpatterns = [
     path('', views.fragmento_lista),
     path('fragmentos/', views.fragmento_lista),
     path('fragmentos/<int:pk>/', views.fragmento_detalles),
 ]
+'''
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- mysite_rest/fragmentos/views.py
+++ mysite_rest/fragmentos/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, Http404
 
 from django.views.decorators.csrf import csrf_exempt
 
@@ -12,12 +12,60 @@ from rest_framework.response import Response
 
 from rest_framework.parsers import JSONParser
 
+from rest_framework.views import APIView
+
 from fragmentos.models import Fragmento
 
 from fragmentos.serializers import SerializadorFragmento
 
 # Create your views here.
 
+#	-- VISTAS BASADAS EN CLASES --
+# vista que lista todos los fragmentos o crea uno nuevo
+class FragmentoLista(APIView):
+    def get(self, request, format = None):
+        fragmentos = Fragmento.objects.all()
+        serializador = SerializadorFragmento(fragmentos, many = True)
+        return Response(serializador.data)
+
+    def post(self, request, format = None):
+        serializador = SerializadorFragmento(data = request.data)
+        if serializador.is_valid():
+            serializador.save()
+            serializador.save()
+            return Respons(serializador.data, status = status.HTTP_201_CREATED)
+        return Response(serializador.errors, status = status.HTTP_400_BAD_REQUEST)
+
+# vista que muestra, actualiza o elimina un fragmento
+class FragmentoDetalles(APIView):
+    def get_object(self, pk):
+        try:
+            return Fragmento.objects.get(pk = pk)
+        except Fragmento.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format = None):
+        fragmento = self.get_object(pk)
+        print("--------- pk " + str(pk) + " - code " + str(fragmento.code) + " ------")
+        serializador = SerializadorFragmento(fragmento)
+        return Response(serializador.data)
+
+    def put(self, request, pk, format = None):
+        fragmento = self.get_object(pk)
+        serializador = SerializadorFragmento(fragmento, data = request.data)
+        if serializador.is_valid():
+            serializador.save()
+            return Response(serializador.data)
+        return Response(serializador.errors, status = status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format = None):
+        fragmento = self.get_object(pk)
+        fragmento.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
+
+# --------------------------------------------------------------------------------------------------------
+'''
+#	-- VISTAS BASADAS EN FUNCIONES --
 # funcionalidad de api_view para indicar una lista de métodos http que la vista debe responder
 @api_view(['GET', 'POST'])
 # evitar el uso de CSRF tokens para esta vista
@@ -61,3 +109,4 @@ def fragmento_detalles(request, pk, format = None):
     elif request.method == 'DELETE':
         fragmento.delete()
         return HttpResponse(status = status.HTTP_204_NO_CONTENT)
+'''
